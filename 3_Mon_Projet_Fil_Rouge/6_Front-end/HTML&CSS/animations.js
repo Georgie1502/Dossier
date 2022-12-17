8c737306dfc22d85d5805cc0105d7056318c3713
Georgie1502 <jor_tejeda@hotmail.com>
2022-12-17 18:18:57 +0100

    js

--- 3_Mon_Projet_Fil_Rouge/6_Front-end/HTML&CSS/animations.js
+++ 3_Mon_Projet_Fil_Rouge/6_Front-end/HTML&CSS/animations.js
@@ -19,3 +19,15 @@ gsap.from(".box", {
       });
     });
   });
+
+
+  const words = ["Plaquistes","Electriciennes","Plombières","Menuisières"];
+
+  let masterTL = gsap.timeline({repeat: -1})
+
+  words.forEach(word => {
+    let tl = gsap.timeline({ repeat : 1, yoyo:true})
+    tl.to('.text',{duration: 1, text: word})
+    masterTL.add(tl)
+   
+  } )
