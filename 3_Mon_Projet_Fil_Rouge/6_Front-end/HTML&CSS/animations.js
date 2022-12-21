2130f03428be4338f1503c49c24d2aefca0170e3
Georgie1502 <108793151+Georgie1502@users.noreply.github.com>
2022-12-21 14:35:28 +0100

    vacan

--- 3_Mon_Projet_Fil_Rouge/6_Front-end/HTML&CSS/animations.js
+++ 3_Mon_Projet_Fil_Rouge/6_Front-end/HTML&CSS/animations.js
@@ -1,11 +1,15 @@
-
-const words = ["Plaquistes","Electriciennes","Plombières","Menuisières"];
+const words = ["Plaquistes","Electriciennes",
+"Plombières","Menuisières"];
 
 let masterTL = gsap.timeline({repeat: -1});
 
 words.forEach(word => {
-    let tl = gsap.timeline({ repeat : 1, yoyo:true})
+    let tl = gsap.timeline({
+    repeat : 1, yoyo:true})
     tl.to('.text',{duration: 1, text: word})
-    masterTL.add(tl)
-   
-  } );
+    masterTL.add(tl)} );
+
+
+
+
+
